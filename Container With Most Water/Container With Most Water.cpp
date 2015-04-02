683369224fad9cf53adac1fbef880e98ac568d2c
lemon <lemon_wonder@163.com>
2015-04-02 21:29:05 +0800

    Container With Most Water

---
+++ Container With Most Water/Container With Most Water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int start = 0;
+        int end = height.size() - 1;
+        int result = INT_MIN;
+        while(start < end)
+        {
+        	int area = min(height[start], height[end]) * (end -start);
+        	result = max(result, area);
+        	if(height[start] <= height[end])
+        	{
+        		start++;
+        	}
+        	else
+        	{
+        		end--;
+        	}
+        }
+
+        return result;
+    }
+};
