0bfab564692b52af1ec118b92d68fc582646b737
lemon0910 <lemon_wonder@163.com>
2015-05-06 10:50:58 +0800

    Combination Sum

---
+++ Combination Sum/Combination Sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        set<vector<int> > result;
+        sort(candidates.begin(), candidates.end());
+        vector<int> values;
+        findResult(candidates, result, 0, values, 0, target);
+        
+        return vector<vector<int> >(result.begin(), result.end());
+    }
+    
+    void findResult(vector<int>& candidates, set<vector<int> > &result, int index, vector<int> values, int sum, int target)
+    {
+        if(sum > target)
+            return;
+        if(sum == target)
+        {
+            result.insert(values);
+            return;
+        }
+        if(index >= candidates.size())
+            return;
+        
+        int cur = sum;
+        vector<int> temp = values;
+        while(true)
+        {
+            findResult(candidates, result, index + 1, temp, cur, target);
+            if(cur + candidates[index] <= target)
+            {
+                cur += candidates[index];
+                temp.push_back(candidates[index]);      
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+};
