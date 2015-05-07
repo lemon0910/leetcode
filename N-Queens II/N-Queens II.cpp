5e206c9480cc35d845d7da96833aa8b11f15da79
lemon0910 <lemon_wonder@163.com>
2015-05-07 10:48:00 +0800

    coding

---
+++ N-Queens II/N-Queens II.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool isValid(vector<string> &board, int x, int y) 
+    {
+        for (int i = 0; i < x; ++i) 
+        {
+            if (board[i][y] == 'Q') return false;
+        }
+
+        for (int i = 0; i < board.size(); ++i) 
+        {
+            for (int j = 0; j < board.size(); ++j) 
+            {
+                if (i != x && j != y && i-j == x-y && board[i][j] == 'Q')
+                    return false;
+                if (i != x && j != y && i+j == x+y && board[i][j] == 'Q')
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    void solveHelper(vector<vector<string> > &res, vector<string> &board, int idx) 
+    {
+        if (idx == board.size()) 
+        {
+            res.push_back(board);
+            return;
+        }
+        for (int i = 0; i < board.size(); ++i) 
+        {
+            board[idx][i] = 'Q';
+            if (isValid(board, idx, i)) 
+            {
+                solveHelper(res, board, idx + 1);
+            }
+            board[idx][i] = '.';
+        }
+    }
+    
+    int totalNQueens(int n) 
+    {
+        vector<vector<string> > res;
+        vector<string> board;
+        string row;
+        for (int i = 0; i < n; ++i) 
+        {
+            row.push_back('.');
+        }
+        for (int i = 0; i < n; ++i) 
+        {
+            board.push_back(row);
+        }
+        solveHelper(res, board, 0);
+        return res.size();
+    }
+};
